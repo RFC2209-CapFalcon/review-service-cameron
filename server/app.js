77fedb4e922f2318f844ba0d0ca4078ae7debfbc
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-11-11 12:52:31 -0700

    Added loader.io verification route

--- server/app.js
+++ server/app.js
@@ -15,6 +15,10 @@ if (process.env.DEV) {
 // Mounts routes via function returned in routes/index.js
 mountRoutes(app)
 
+// Route for Loader.io Verification
+app.get('/loaderio-d71a4a0b09f4605ceb47e30636f12c67' (req, res) => {
+  res.send('loaderio-d71a4a0b09f4605ceb47e30636f12c67')
+})
 
 app.get('*', (req, res) => {
   res.status(404).send('Resource not found')
